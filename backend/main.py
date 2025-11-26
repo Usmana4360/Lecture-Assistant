2804e02096302844a91e65a93fc6a2adda751508
Muhammad Usman <usmanashraf4360@gmail.com>
2025-11-26 16:13:29 +0500

    Update main

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(title="Lecture-Assistant Agent API")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://lectureassistant1.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
